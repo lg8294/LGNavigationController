fa6a37bf1eb65c4547ad700ce1001ee75ce51673
arrcen-lg <515930926@qq.com>
2017-06-09 13:55:03 +0800

    Initial Commit

---
+++ LGNavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGNavigationController
+//
+//  Created by iOS on 2017/6/9.
+//  Copyright © 2017年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LGNavigationController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LGNavigationController
+//
+//  Created by iOS on 2017/6/9.
+//  Copyright © 2017年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
